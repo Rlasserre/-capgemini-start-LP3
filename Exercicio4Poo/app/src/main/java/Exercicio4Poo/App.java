0de210e5effb3b701c4ff99035a4c5e6f6516ed4
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-05-31 11:08:02 -0300

    Update project ExercicioPoo

--- Exercicio4Poo/app/src/main/java/Exercicio4Poo/App.java
+++ Exercicio4Poo/app/src/main/java/Exercicio4Poo/App.java
@@ -5,9 +5,9 @@ package Exercicio4Poo;
 
 public class App {
     
-    }
-
     public static void main(String[] args) {
-
+    
+        Invoice inv = new Invoice(1, "Mousepad", -10, 50.0f);
+        System.err.println("Valor total: " + inv.getInvoiceAmout());
     }
 }
