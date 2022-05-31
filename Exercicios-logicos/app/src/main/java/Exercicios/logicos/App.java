aab614fd292877aa152edf62e2ff5aa8f9ece560
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-05-31 10:22:13 -0300

    Update App.java

--- Exercicios-logicos/app/src/main/java/Exercicios/logicos/App.java
+++ Exercicios-logicos/app/src/main/java/Exercicios/logicos/App.java
@@ -8,21 +8,45 @@ public class App {
     public static void main(String[] args) {
         
         int anoFabricacao = 0;
-        float valorCarro = 0.0f;
+        float valorVeiculo = 0.0f;
+        float porcentagemDesconto = 0.0f;
+        float valorDesconto = 0.0f;
+        float valorPagar = 0.0f;
+        
+        int totalCarrosSemiNovos = 0;
+        int totalCarros = 0;
         
         Scanner leitor = new Scanner(System.in);
         
-        boolean desejaRepetir = true;
+        char desejaRepetir = 's';
         
-        while (desejaRepetir == true) {
+        while (desejaRepetir == 's' || desejaRepetir == 'S') {
             
+            //entrada de dados
             System.out.println("Digite o ano de fabricação do veículo");
             anoFabricacao = leitor.nextInt();
             
             System.out.println("Digite o valor do carro");
-            valorCarro = leitor.nextFloat();
+            valorVeiculo = leitor.nextFloat();
                 
+            if (anoFabricacao >= 2000) {
+                porcentagemDesconto = 0.12f;
+            } else { 
+                porcentagemDesconto = 0.07f;
+                totalCarrosSemiNovos ++;
+            }
+            totalCarros ++;
+            
+            valorDesconto = valorVeiculo * porcentagemDesconto;
+            
+            System.err.println("O valor de desconto foi de: " + valorDesconto);
+            System.err.println("O valor que deve ser pago é de: " + valorPagar);
+            
+            System.out.println("Deseja fazer mais cadastros? S - sim / N - não");
+            desejaRepetir = leitor.next().charAt(0);
         }
         
+        System.out.println("Total de carros semi novos: " + totalCarrosSemiNovos);
+        System.out.println("Total de carros: " + totalCarros);
     }
 }
